3fd3c80eb823b7169972d8158c1c2aa24e91634f
shubham491 <shubham.das@students.iiit.ac.in>
2020-07-28 11:23:43 +0530

    2nd commit

--- pkg/APIUtil/APIUtil.go
+++ pkg/APIUtil/APIUtil.go
@@ -2,51 +2,39 @@ package APIUtil
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+
+	//"encoding/json"
+	"fmt"
+	//"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 
 	//"github.com/jyotishp/order-analysis/pkg/ErrorHandlers"
-	"github.com/shubham491/order-analysis/pkg/Models"
-	"io/ioutil"
-	"net/http"
-	"os"
+	//"github.com/shubham491/order-analysis/pkg/Models"
+	//"io/ioutil"
+	//"net/http"
+	//"os"
 	"sort"
 	"strconv"
-	"github.com/shubham491/order-analysis/pkg/AuthUtil"
+	//"github.com/shubham491/order-analysis/pkg/AuthUtil"
 	"github.com/shubham491/order-analysis/pkg/services/orders/orderspb"
 )
 
-var Restaurant_count = make(map[string]int)
-var Cuisine_count = make(map[string]int)
-var State_cuisine_count = make(map[string]map[string]int)
-var Orders = make(map[string] int)
+var Restaurant_count = make(map[string] int64)
+var Cuisine_count = make(map[string] int64)
+var State_cuisine_count = make(map[string]map[string]int64)
+var Orders = make(map[string] int64)
 
 type KV struct {
 	Key   string
 	Value int
 }
 
-type orders_server struct {
+type OrdersServiceServer struct {
 
-}
-
-func main()  {
-	lis, err := net.Listen("tcp", "0.0.0.0:50051")
-	if err != nil {
-		log.Fatalf("Sorry failed to load server %v: ", err)
-	}
-
-	s := grpc.NewServer()
-
-	orderspb.RegisterOrdersServiceServer(s, &orders_server{})
-	fmt.Println("Orders Server starting...")
-	if s.Serve(lis); err != nil {
-		log.Fatalf("failed to Serve %v", err)
-	}
 }
 
 func KeySort(count map[string] int, num string) []KV{
@@ -79,28 +67,29 @@ func KeySort(count map[string] int, num string) []KV{
 
 
 
-func (*orders_server) GetAllRestaurants(ctx context.Context, req *orderspb.AllCuisineRequest) (*orderspb.AllRestaurantResponse, error)  {
-	res:=Restaurant_count
+func (s *OrdersServiceServer) GetAllRestaurant(ctx context.Context, request *orderspb.AllRestaurantRequest) (*orderspb.AllRestaurantResponse, error) {
+	res:=&orderspb.AllRestaurantResponse{AllRestaurant:Restaurant_count}
 	return res, nil
 }
-func GetAllCusines(c *gin.Context) {
-
-	user := c.MustGet(gin.AuthUserKey).(string)
-	if _, ok := AuthUtil.Secrets[user]; ok {
-		c.JSON(200, Cuisine_count)
-	} else {
-		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-	}
+func (s *OrdersServiceServer) GetAllCuisine(ctx context.Context, request *orderspb.AllCuisineRequest) (*orderspb.AllCuisineResponse, error) {
+	res:=&orderspb.AllCuisineResponse{AllCuisine:Cuisine_count}
+	return res, nil
 }
 
-func GetAllStatesCuisines(c *gin.Context) {
+func (s *OrdersServiceServer) GetAllState(ctx context.Context, request *orderspb.AllStateRequest) (*orderspb.AllStateResponse, error) {
+	var res *orderspb.AllStateResponse
+	var res1 *orderspb.AllCuisine
+	var tempMap=make(map[string]*orderspb.AllCuisine)
+	for k,v:= range State_cuisine_count{
+		res1= &orderspb.AllCuisine{
+			AllCuisine:v,
+		}
 
-	user := c.MustGet(gin.AuthUserKey).(string)
-	if _, ok := AuthUtil.Secrets[user]; ok {
-		c.JSON(200, State_cuisine_count)
-	} else {
-		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		tempMap[k]=res1
+
 	}
+	res=&orderspb.AllStateResponse{AllState:tempMap}
+	return res, nil
 }
 
 
@@ -121,119 +110,134 @@ func GetTopNumRestaurants(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 	}
 }
+//
+//
+//
+//func GetTopNumStatesCuisines(c *gin.Context) {
+//
+//	user := c.MustGet(gin.AuthUserKey).(string)
+//	if _, ok := AuthUtil.Secrets[user]; ok {
+//
+//		num := c.Param("num")
+//		state := c.Param("state")
+//		jsonSlice:= KeySort(State_cuisine_count[state], num)
+//		if jsonSlice == nil{
+//			c.JSON(200,gin.H{
+//				"Error":"Provide valid integer value.",
+//			})
+//		} else {
+//			c.JSON(200, jsonSlice)
+//		}
+//	} else {
+//		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+//	}
+//}
+//
+//func GetTopNumCuisines(c *gin.Context) {
+//
+//	user := c.MustGet(gin.AuthUserKey).(string)
+//	if _, ok := AuthUtil.Secrets[user]; ok {
+//		num := c.Param("num")
+//		jsonSlice:= KeySort(Cuisine_count, num)
+//		if jsonSlice == nil{
+//			c.JSON(200,gin.H{
+//				"Error":"Provide valid integer value.",
+//			})
+//		} else {
+//			c.JSON(200, jsonSlice)
+//		}
+//	} else {
+//		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+//	}
+//}
+//
+//func CheckError(err error, c *gin.Context)  {
+//	if err != nil {
+//		c.JSON(200,gin.H{
+//			"error":err.Error(),
+//		})
+//	}
+//}
+//
+//func AddOrder(c *gin.Context){
+//	user := c.MustGet(gin.AuthUserKey).(string)
+//	if _, ok := AuthUtil.Secrets[user]; ok {
+//	body:=c.Request.Body
+//	content, _:= ioutil.ReadAll(body)
+//	var orderData Models.Order
+//	var orderData2 Models.Order
+//	err := json.Unmarshal([]byte(content), &orderData)
+//	CheckError(err,c)
+//	err = json.Unmarshal(content, &orderData2)
+//	CheckError(err,c)
+//	Id := string(orderData2.Id)
+//	fmt.Println(orderData2)
+//	if Orders[string(Id)] == 1{
+//		c.JSON(200, gin.H{
+//			"Error":"Order ID already there",
+//		})
+//		return
+//	}
+//
+//	f, err := os.OpenFile("outputs.json", os.O_RDWR, os.ModePerm)
+//	defer f.Close()
+//	CheckError(err,c)
+//
+//	orderJson, err := json.Marshal(orderData)
+//	CheckError(err,c)
+//
+//	orderString := string(orderJson)
+//	orderString = "," + orderString
+//
+//	off := int64(2)
+//	stat, err := os.Stat("outputs.json")
+//	fmt.Println("Size : ", stat.Size())
+//	start := stat.Size() - off
+//
+//	tmp := []byte(orderString)
+//	_, err = f.WriteAt(tmp, start)
+//	CheckError(err, c)
+//
+//	str := []byte("]}")
+//	_, err = f.WriteAt(str, start + int64(len(orderString)))
+//	CheckError(err, c)
+//
+//	restaurant := orderData.RestName
+//	cuisine := orderData.Cuisine
+//	state := orderData.State
+//
+//	Restaurant_count[restaurant]++
+//	Cuisine_count[cuisine]++
+//	Orders[string(Id)]++
+//	statemap, ok := State_cuisine_count[state]
+//	if ok {
+//		statemap[cuisine]++
+//	} else {
+//		State_cuisine_count[state] = make(map[string]int)
+//		State_cuisine_count[state][cuisine]++
+//	}
+//
+//	c.JSON(200,gin.H{
+//		"success":"order successfully added",
+//	})
+//
+//	} else {
+//		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+//	}
+//}
 
-
-
-func GetTopNumStatesCuisines(c *gin.Context) {
-
-	user := c.MustGet(gin.AuthUserKey).(string)
-	if _, ok := AuthUtil.Secrets[user]; ok {
-
-		num := c.Param("num")
-		state := c.Param("state")
-		jsonSlice:= KeySort(State_cuisine_count[state], num)
-		if jsonSlice == nil{
-			c.JSON(200,gin.H{
-				"Error":"Provide valid integer value.",
-			})
-		} else {
-			c.JSON(200, jsonSlice)
-		}
-	} else {
-		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-	}
-}
-
-func GetTopNumCuisines(c *gin.Context) {
-
-	user := c.MustGet(gin.AuthUserKey).(string)
-	if _, ok := AuthUtil.Secrets[user]; ok {
-		num := c.Param("num")
-		jsonSlice:= KeySort(Cuisine_count, num)
-		if jsonSlice == nil{
-			c.JSON(200,gin.H{
-				"Error":"Provide valid integer value.",
-			})
-		} else {
-			c.JSON(200, jsonSlice)
-		}
-	} else {
-		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-	}
-}
-
-func CheckError(err error, c *gin.Context)  {
+func main()  {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		c.JSON(200,gin.H{
-			"error":err.Error(),
-		})
-	}
-}
-
-func AddOrder(c *gin.Context){
-	user := c.MustGet(gin.AuthUserKey).(string)
-	if _, ok := AuthUtil.Secrets[user]; ok {
-	body:=c.Request.Body
-	content, _:= ioutil.ReadAll(body)
-	var orderData Models.Order
-	var orderData2 Models.Order
-	err := json.Unmarshal([]byte(content), &orderData)
-	CheckError(err,c)
-	err = json.Unmarshal(content, &orderData2)
-	CheckError(err,c)
-	Id := string(orderData2.Id)
-	fmt.Println(orderData2)
-	if Orders[string(Id)] == 1{
-		c.JSON(200, gin.H{
-			"Error":"Order ID already there",
-		})
-		return
+		log.Fatalf("Sorry failed to load server %v: ", err)
 	}
 
-	f, err := os.OpenFile("outputs.json", os.O_RDWR, os.ModePerm)
-	defer f.Close()
-	CheckError(err,c)
+	s := grpc.NewServer()
 
-	orderJson, err := json.Marshal(orderData)
-	CheckError(err,c)
-
-	orderString := string(orderJson)
-	orderString = "," + orderString
-
-	off := int64(2)
-	stat, err := os.Stat("outputs.json")
-	fmt.Println("Size : ", stat.Size())
-	start := stat.Size() - off
-
-	tmp := []byte(orderString)
-	_, err = f.WriteAt(tmp, start)
-	CheckError(err, c)
-
-	str := []byte("]}")
-	_, err = f.WriteAt(str, start + int64(len(orderString)))
-	CheckError(err, c)
-
-	restaurant := orderData.RestName
-	cuisine := orderData.Cuisine
-	state := orderData.State
-
-	Restaurant_count[restaurant]++
-	Cuisine_count[cuisine]++
-	Orders[string(Id)]++
-	statemap, ok := State_cuisine_count[state]
-	if ok {
-		statemap[cuisine]++
-	} else {
-		State_cuisine_count[state] = make(map[string]int)
-		State_cuisine_count[state][cuisine]++
-	}
-
-	c.JSON(200,gin.H{
-		"success":"order successfully added",
-	})
-
-	} else {
-		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+	orderspb.RegisterOrdersServiceServer(s, &OrdersServiceServer{})
+	fmt.Println("Orders Server starting...")
+	if s.Serve(lis); err != nil {
+		log.Fatalf("failed to Serve %v", err)
 	}
 }
 
--- pkg/Grpc_client/Grpc_client.go
+++ pkg/Grpc_client/Grpc_client.go
@@ -3,6 +3,7 @@ package Grpc_client
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/shubham491/order-analysis/pkg/AuthUtil"
+	"github.com/shubham491/order-analysis/pkg/services/orders/orderspb"
 	"google.golang.org/grpc"
 	"log"
 	"net/http"
@@ -10,14 +11,20 @@ import (
 
 func GetAllRestaurants(c *gin.Context) {
 
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Sorry client cannot talk to server: %v: ", err)
-	}
-	defer conn.Close();
 	user := c.MustGet(gin.AuthUserKey).(string)
+
+
 	if _, ok := AuthUtil.Secrets[user]; ok {
-		c.JSON(200, Restaurant_count)
+		conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Sorry client cannot talk to server: %v: ", err)
+		}
+
+		defer conn.Close();
+		oc := orderspb.NewOrdersServiceClient(conn)
+		req := &orderspb.AllRestaurantRequest{}
+		res, err := oc.GetAllRestaurant(c, req)
+		c.JSON(200,res.GetAllRestaurant())
 	} else {
 		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 	}
@@ -27,18 +34,65 @@ func GetAllRestaurants(c *gin.Context) {
 func GetAllCusines(c *gin.Context) {
 
 	user := c.MustGet(gin.AuthUserKey).(string)
+
+
 	if _, ok := AuthUtil.Secrets[user]; ok {
-		c.JSON(200, Cuisine_count)
+		conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Sorry client cannot talk to server: %v: ", err)
+		}
+
+		defer conn.Close();
+		oc := orderspb.NewOrdersServiceClient(conn)
+		req := &orderspb.AllCuisineRequest{}
+		res, err := oc.GetAllCuisine(c, req)
+		c.JSON(200,res.GetAllCuisine())
 	} else {
 		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 	}
+
 }
+
 
 func GetAllStatesCuisines(c *gin.Context) {
 
 	user := c.MustGet(gin.AuthUserKey).(string)
+
+
 	if _, ok := AuthUtil.Secrets[user]; ok {
-		c.JSON(200, State_cuisine_count)
+		conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Sorry client cannot talk to server: %v: ", err)
+		}
+
+		defer conn.Close();
+		oc := orderspb.NewOrdersServiceClient(conn)
+		req := &orderspb.AllStateRequest{}
+		res, err := oc.GetAllState(c, req)
+		c.JSON(200,res.GetAllState())
+	} else {
+		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+	}
+
+}
+
+func GetTopNumRestaurants(c *gin.Context) {
+
+	user := c.MustGet(gin.AuthUserKey).(string)
+
+
+	if _, ok := AuthUtil.Secrets[user]; ok {
+		num := c.Param("num")
+		conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Sorry client cannot talk to server: %v: ", err)
+		}
+
+		defer conn.Close();
+		oc := orderspb.NewOrdersServiceClient(conn)
+		req := &orderspb.AllStateRequest{}
+		res, err := oc.GetAllState(c, req)
+		c.JSON(200,res.GetAllState())
 	} else {
 		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 	}
